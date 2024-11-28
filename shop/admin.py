6ce8d8acb5af48df6257dfbb8c4b2e29c4d2681a
stranger <dctngamers@gmail.com>
2024-11-28 12:09:55 +0530

    completed

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user","product","color","size"]
+    search_fields = ["user","product","color","size"]
 admin.site.register(Products)
 admin.site.register(Cart)
 admin.site.register(Colors)
-admin.site.register(Order)
+admin.site.register(Order,OrderAdmin)
